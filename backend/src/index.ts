3dca8853051fdd60e67e33a20b584b3c75d17b6c
Naman Kundra <namankundra6@gmail.com>
2024-08-02 20:59:35 +0530

    compiler added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import problemsRoutes from "./routes/problems"
 import submissionRouter from "./routes/submission";
 import userRouter from "./routes/userRoutes";
+import ide from "./routes/compiler"
 
 dotenv.config();
 
@@ -21,7 +22,7 @@ app.use(cors());
 app.use("/api/problems", problemsRoutes);
 app.use("/api/submission", submissionRouter);
 app.use("/api/user",userRouter);
-app.use("/api/ide")
+app.use("/api/ide",ide)
 
 app.listen(7777,()=>{
     console.log("Server is running on port 7777");
