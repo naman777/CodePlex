0e34dc41820eed73ba31ba679a6762a16a875bbf
Naman Kundra <namankundra6@gmail.com>
2024-08-05 18:26:44 +0530

    improve backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: true,
     credentials: true, 
 };
 
